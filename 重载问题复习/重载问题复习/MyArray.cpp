5ea3b6d85ccf1d845b5af97a6e6b0a14560aaf0c
shiji-lu <766993892@qq.com>
2023-05-01 23:37:03 +0800

    1

---
+++ 重载问题复习/重载问题复习/MyArray.cpp
@@ -0,0 +1,63 @@
+#include "MyArray.h"
+MyArray::MyArray()
+{
+
+}
+MyArray::MyArray(const MyArray& arr)
+{
+
+}
+MyArray::MyArray(int capacity, int val = 0)
+{
+
+}
+MyArray::~MyArray()
+{
+
+}
+
+//头插
+void MyArray::PushFront(int val)
+{
+
+}
+//尾插
+void MyArray::PushBack(int val)
+{
+
+}
+//头删
+void MyArray::PopFront()
+{
+
+}
+//尾删
+void MyArray::PopBack()
+{
+
+}
+//获取数组元素个数
+int MyArray::Size()
+{
+
+}
+//获取数组容量
+int MyArray::Capacity()
+{
+
+}
+//指定位置插入yuans 
+void MyArray::Insert(int pos, int val)
+{
+
+}
+//获取指定位置的值
+int& MyArray::Get(int pos)
+{
+
+}
+//在指定位置修改值
+void MyArray::Set(int pos, int val)
+{
+
+}
