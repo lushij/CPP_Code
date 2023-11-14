26c4eceaeef74b2d0c684979a6f7595408126c14
lushij <766993892@qq.com>
2023-11-14 23:02:40 +0800

    1

---
+++ 电梯案例/电梯案例/main.cpp
@@ -0,0 +1,35 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<iostream>
+#include <cstring>
+#include <algorithm>
+#include<list>
+#include<vector>
+#include<queue>
+#include<string>
+using namespace std;
+/*
+电梯案例
+1.创建人，每一层进出电梯的人和数量
+2.进电梯
+3.出电梯
+
+
+
+
+*/
+
+//人员类
+class People
+{
+public:
+	string name;
+};
+
+int main()
+{
+	list<People>mylist;//创建电梯
+	vector<People>V;
+
+
+	return 0;
+}
